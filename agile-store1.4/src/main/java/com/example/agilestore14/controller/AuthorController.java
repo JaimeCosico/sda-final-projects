fe19731a7130deb79768d218e2a0669f02beb171
Jaime Daniel M. Cosico <mapfrejm2cea@gmail.com>
2021-05-21 10:55:31 +0800

    -Created Repository interfaces:CategoryRepository and OrdersRepository

--- agile-store1.4/src/main/java/com/example/agilestore14/controller/AuthorController.java
+++ agile-store1.4/src/main/java/com/example/agilestore14/controller/AuthorController.java
@@ -4,6 +4,7 @@ import com.example.agilestore14.services.AuthorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
